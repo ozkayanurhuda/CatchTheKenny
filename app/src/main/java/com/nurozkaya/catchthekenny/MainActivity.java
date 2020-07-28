65a42a1f7175e779a613c17e107ea509ff2e23fd
ozkayanurhuda <46482358+ozkayanurhuda@users.noreply.github.com>
2020-07-28 15:48:08 +0300

    Initial commit

---
+++ app/src/main/java/com/nurozkaya/catchthekenny/MainActivity.java
@@ -0,0 +1,122 @@
+package com.nurozkaya.catchthekenny;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView scoreText; //objeleri burda tanımladım
+    TextView timeText;
+    int score;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeText = (TextView)findViewById(R.id.timeText);
+        scoreText = (TextView)findViewById(R.id.scoreText);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
+        hideImages();
+
+
+        score = 0;
+
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long l) { // her bir saniyede
+                timeText.setText("Time: "+l/1000);
+            }
+
+            @Override
+            public void onFinish() { // bitince
+                timeText.setText("Time Off!");
+                handler.removeCallbacks(runnable);
+                for (ImageView image: imageArray) { // zaman dolunca kennyi saklayan loop
+                    image.setVisibility(View.INVISIBLE);
+                }
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Restart ? ");
+                alert.setMessage("Are you sure to restart game ? ");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() { // yes dersek  ne olacak
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { // kendi açıyor
+                        // restart
+                        Intent intent= getIntent();
+                        finish(); // güncel akvititeyi bitirecek yeni başlatacak
+                        startActivity(intent);
+
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Game Over",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+
+
+            }
+        }.start(); //yazmak zorundasın
+    }
+
+    public void increaseScore(View view) {
+        score++;
+        scoreText.setText("Score: "+score);
+
+    }
+    public void hideImages () {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image: imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(9); // 0 ile 8 arasında rastgele rakam getirir
+                imageArray[i].setVisibility(View.VISIBLE); //i. elemanı görünür hale getirir.
+                handler.postDelayed(this,500); // hangi periyotta çalışacağı , burdaki this runnable a işaret ediyor.500 yarım saniye
+
+            }
+        };
+        handler.post(runnable);
+
+
+    }
+}
